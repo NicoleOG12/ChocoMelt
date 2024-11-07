5630611edded9d0d6596b99646e01f615a9b0844
nicole <nicole.goncalves6@etec.sp.gov.br>
2024-11-07 15:33:08 -0300

    Correções

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,87 @@
 <template>
-
-  <!-- imagem do nome + menu-->
-  <header>
-
-      <!--imagem do nome-->
-      <div id="imagemdonome"></div>
-
-      <!--barra de menu-->
-      <div id="barrademenu">
-
-        <!--menu-->
-        <a href="" id="menu">
-            <p>MENU</p>
-        </a>
-
-        <!--barra de pesquisa-->
-        <div id="">
-          <input type="text" name="Pesquisar" id="input" value="Pesquisar">
-        </div>
-        
-        <!--área de login-->
-        <a href="components/HomePage.vue" id="areadelogin">
-            <p>ENTRAR</p>
-        </a>
-        
-      </div>
-
+  <div id="app">
+    <header class="menu">
+      <nav>
+        <ul>
+          <li><router-link to="/HomePage">Home Page</router-link></li>
+          <li><router-link to="/contactUs">Contate-nos</router-link></li>
+          <li><router-link to="/aboutUS">Sobre nós</router-link></li>
+          <li><router-link to="/LoginForm">Login</router-link></li>
+          <li><router-link to="/NewPage">NewPage</router-link></li>
+          
+        </ul>
+      </nav>
     </header>
 
+    <router-view></router-view>
+  </div>
 
-
-
+  <div class="footer">
+      <footer>
+        <div>
+          <p class="copyright" >Copyright © 2024</p>
+        </div>
+      </footer>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'App',
+}
 </script>
 
 <style>
-
-/*cores: #D4A3C4 #A8D1E7 #FCFAF2 */
-
-P, a, a:any-link, a:visited, a:link{
-  margin:0px;
-  color:inherit;
-  text-decoration:none;
-  text-decoration-color:none;
-}
-
-html{
-  padding:0px;
-}
-
-header{
-  margin: 0px;
-  position: absolute;
-  right: 0px;  left: 0px;  top: 0px;
-}
-
-  #imagemdonome{
-    height: 300px;
-    background-image: url("https://mir-s3-cdn-cf.behance.net/project_modules/1400/18d1d2204449029.66a91bc06488f.png");
-    background-size: cover;
-  }
-
-  #barrademenu{
-    background-image: linear-gradient(#b8dcef, #A8D1E7);
-    display: grid;
-    grid-template-columns: 10% auto 12%;
-    padding: 16px;
-    color:#fcfaf2;
-    text-align: center;
-    align-items: center;
-  }
-
-  /* From Uiverse.io by shadowmurphy */ 
-  #input {
-  border: 2px solid transparent;
-  width: 30em;
-  height: 2.5em;
-  padding-left: 0.8em;
-  outline: none;
-  overflow: hidden;
-  background-color: #ffffff;
-  border-radius: 10px;
-  transition: all 0.5s;
-  }
-
-  #input:hover,
-  #input:focus {
-  border: 2px solid #4A9DEC;
-  box-shadow: 0px 0px 0px 7px rgb(74, 157, 236, 20%);
-  background-color: white;
-  }
-
-   /* #barradepesquisa{
-      background-image:linear-gradient(#fcfaf241,#fcfaf253);
-      margin: 0px 6% 0 6%;
-      padding: 6px 6px 6px 16px;
-      border-radius: 16px;
-      text-align: left;
-      width: 400px;
-      background-color: #A8D1E7;
-      text-decoration: none;
-    } */
-
-
-
-
-
-/*
 #app {
+  background-image: url(https://images.app.goo.gl/tUkbf2CLc8V7gjVS6);
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 20px;
 }
-*/
 
+.menu {
+  background-color: rgba(255, 255, 255, 0.8); 
+  padding: 10px; 
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); 
+  margin-top: -50px;
+}
+
+.menu nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.menu nav ul li {
+  display: inline;
+  margin-right: 15px; 
+}
+
+.menu nav ul li a {
+  text-decoration: none;
+  color: #000000; 
+  font-family: 'Allura';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/allura/v21/9oRPNYsQpS4zjuA_iwgW.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+.footer {
+  padding: 20px 0;
+  background-color:#ffffff;
+  color:#000000;
+  margin-top: 10px;
+}
+
+.footer .copyright {
+  margin-top:14px;
+  margin-bottom:0;
+  font-size:13px;
+  opacity:0.6;
+}
 </style>
---
+++ src/components/CarrinhoPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="chocomelt">
+      <header>
+        <h1 class="logo">CHOCOMELT</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Carrinho</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Sobre Nós</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  </template>
+<script>
+</script>

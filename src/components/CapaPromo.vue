5645fa90c36f0233d73e049d0b92186816818eed
BrunoCampxs <bruno.andrade115@etec.sp.gov.br>
2024-11-29 18:00:02 -0300

    queen never cry

---
+++ src/components/CapaPromo.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="promo">
+    <h1>Aaaa</h1>
+    <p>seilameo</p>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+.promo{
+    width: 220px;
+    height: 120px;
+    background-color: rgb(156, 73, 86);
+}
+
+h1{
+    color: red;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,102 +1,99 @@
 <template>
-  <body id="homepage">
-    <header>
-      <nav>
-        <!--<ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Produtos</a></li>
-          <li><a href="#">Carrinho</a></li>
-          <li><a href="#">Contato</a></li>
-          <li><a href="#">Sobre Nós</a></li>
-        </ul>-->
-      </nav>
-    </header>
+<body id="homepage">
+	<header>
+		<nav>
+			<!--<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Produtos</a></li>
+			<li><a href="#">Carrinho</a></li>
+			<li><a href="#">Contato</a></li>
+			<li><a href="#">Sobre Nós</a></li>
+			</ul>-->
+		</nav>
+	</header>
 
-
-<div class="container">
-  <div class="items">
-    <div class="item" style="background-color: blue;">
-
-
-      <div id="promocoes">
-  <div style="background-color: blue;"></div>
-  <div style="background-color: red;"></div>
-  <div style="background-color: green;"></div>
-  <div style="background-color: purple;"></div>
-  <div style="background-color: greenyellow;"></div>
-</div>
-
-      <div id="promocoes" style="margin-left:1100px; margin-top:-120px">
-  <div style="background-color: blue;"></div>
-  <div style="background-color: red;"></div>
-  <div style="background-color: green;"></div>
-  <div style="background-color: purple;"></div>
-  <div style="background-color: greenyellow;"></div>
-</div>
-    </div>
-  </div>
-</div>
-
-
-
-
+	<!--barra de menu-->
+	<div id="barra-de-menu">
+		<!--menu-->	
 		
-		<!--barra de menu-->
-		<div id="barra-de-menu">
+				
 		
-			<!--menu-->
+		<!-- barra de pesquisa-->
+		<a href="" id="barra-de-pesquisa"> 
+			<p>Pesquisar...</p>
+		</a>
 		
-				<div id="menu-dropdown">
-					<span> MENU </span>
-						<div id="menu-dropdown-conteudo">
-							<p> P�gina inicial </p>
-							<p> Produtos </p>
-							<p> Paootes </p>
-							<p> Promo��es </p>
-							<p> Pedidos </p>
-							<p> Sobre n�s </p>
-							<p> Contato </p>
-						</div>
+		<!--�rea de login-->
+		<a href="" id="">
+			<p> Carrinho </p>
+		</a>
+		
+		<!--carrinho-->
+		<a href="" id="area-de-login"> 
+			<p>ENTRAR</p>
+		</a>
+	</div>
+
+	<div class="container">
+		<div class="items">
+			<div class="item">
+				<div id="promocoes">
+					<!--<CapaPromo></CapaPromo>-->
+					<div class="capa-promo" style="background-color: blue;">
+						<div class="overlay"> </div>
+					</div>
+					<div class="capa-promo" style="background-color: red;">
+						<div class="overlay"> </div>
+					</div>
+					<div class="capa-promo" style="background-color: blue;">
+						<div class="overlay"> </div>
+					</div>
+					<div class="capa-promo" style="background-color: red;">
+						<div class="overlay"> </div>
+					</div>
+					<div class="capa-promo" style="background-color: blue;">
+						<div class="overlay"> </div>
+					</div>
+					<div class="capa-promo" style="background-color: red;">
+						<div class="overlay"> </div>
+					</div>
 				</div>
 
-			
-			<!-- barra de pesquisa-->
-			<a href="" id="barra-de-pesquisa"> 
-				<p>Pesquisar...</p>
-			</a>
-			
-			<!--�rea de login-->
-			<a href="" id="">
-				<p> Carrinho </p>
-			</a>
-			
-			<!--carrinho-->
-			<a href="" id="area-de-login"> 
-				<p>ENTRAR</p>
-			</a>
-	
+				<div id="promocoes" style="margin-left:1320px; margin-top:-120px">
+
+					<div class="capa-promo" style="background-color: green;"></div>
+					<div class="capa-promo" style="background-color: purple;"></div>
+					<div class="capa-promo" style="background-color: green;"></div>
+					<div class="capa-promo" style="background-color: purple;"></div>
+					<div class="capa-promo" style="background-color: green;"></div>
+					<div class="capa-promo" style="background-color: purple;"></div>
+				</div>
+			</div>
 		</div>
+	</div>
+</body>
 
-
-
-
-  </body>
 </template>
  
 <script>
+import CapaPromo from './CapaPromo.vue';
 
+export default {
+	name: 'HomePage',
+	components: [
+		CapaPromo
+	],
+};
 </script>
  
 <style>
 
 span, p, a:any-link a:visited, a:link{
-			margin: 0px;
-			color: inherit;
-			text-decoration: none;
-			text-decoration-color: none;
-		}
-		
-
+	margin: 0px;
+	color: inherit;
+	text-decoration: none;
+	text-decoration-color: none;
+}
 
 body{
   background-color: #A8D1E7;
@@ -110,76 +107,74 @@ body{
   overflow-x: hidden;
 }
 
+#barra-de-menu{
+	background-image: linear-gradient(#b8dcef, #A8D1E7);
+	display: grid;
+	grid-template-columns: 16% auto 8% 12%;
+	padding: 16px;
+	color: #fcfaf2;
+	text-align: center;
+	align-items: center;
+}
+			
+
+#menu-dropdown-conteudo p{
+	margin: 16px 0px 8px 0px;						
+}
+			
+#barra-de-pesquisa{
+	background-image: linear-gradient(#fcfaf241, #fcfaf253);
+	margin: 0px 5% 0px 6%;
+	padding: 6px 6px 6px 16px;
+	border-radius: 16px;
+	text-align: left;
+}
+
 #promocoes{
   display: grid;
   width:fit-content;
   margin:0px;
-  grid-template-columns:220px 220px 220px 220px 220px;
+  grid-template-columns:220px 220px 220px 220px 220px 220px;
 }
 
-  #promocoes div{
-    height: 120px;
-  }
+#promocoes .capa-promo{
+	height: 120px;
+}
 
-
-.items, .items2{
+.items{
   height: 120px;
   position: relative;
   width: 1100px;
-  background: #efefef;
   text-align: center;
   border-radius: 4px;
-  animation: slide-item 22s infinite linear;
+  animation: slide-item 40s infinite linear;
+}
+.items:hover{
+  animation-play-state: paused;
 }
 
 @keyframes slide-item {
-    0% { left: -0%; right: -1100px; }
-  100% { left: -1100px; right: 0% }
+    0% { left: -0%; right: -1320px; }
+  100% { left: -1320px; right: 0% }
 }
 
-
-		
-
-			#barra-de-menu{
-				background-image: linear-gradient(#b8dcef, #A8D1E7);
-				display: grid;
-				grid-template-columns: 16% auto 8% 12%;
-				padding: 16px;
-				color: #fcfaf2;
-				text-align: center;
-				align-items: center;
-			}
-			
-				#menu-dropdown{
-					position: relative;
-					display: inline-block;
-				}
-				#menu-dropdown:hover #menu-dropdown-conteudo{
-					display: block;
-				}
-					#menu-dropdown-conteudo{
-						display: none;
-						position: absolute;
-						background-color:#A8D1E7;
-						padding: 10px 16px 8px 10%;
-						margin: 16px 16px 0px -16px;
-						width: 100%;
-						text-align: left;
-					}
-					#menu-dropdown-conteudo p{
-						margin: 16px 0px 8px 0px;						
-					}
-				
-				#barra-de-pesquisa{
-					background-image: linear-gradient(#fcfaf241, #fcfaf253);
-					margin: 0px 5% 0px 6%;
-					padding: 6px 6px 6px 16px;
-					border-radius: 16px;
-					text-align: left;
-				}
-		
+.capa-promo div {
+		height:120px;
+		width:220px;
+		transition: all 0.4s ease-in-out;
+		background-image: url('https://static.vecteezy.com/system/resources/previews/029/720/177/non_2x/mixed-fruit-transparent-background-png.png');
+		background-size: cover;
+		background-position: center;
+		position:relative;
+		cursor: pointer;
+	}
 	
-
+.capa-promo div:hover {
+		height:124%;
+		width:124%;
+		margin:-12%;
+		position:relative;
+	}
 
 
 </style>
